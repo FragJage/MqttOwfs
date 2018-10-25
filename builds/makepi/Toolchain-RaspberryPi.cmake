6a664b38b6dffe4ca6c663adf2cb15984aa273c8
FragJage <fitrec.jage@free.fr>
2018-10-25 23:55:24 +0200

    Cross compil on gnueabihf

--- builds/makepi/Toolchain-RaspberryPi.cmake
+++ builds/makepi/Toolchain-RaspberryPi.cmake
@@ -1,19 +1,22 @@
-# this one is important
+# Target system
 SET(CMAKE_SYSTEM_NAME Linux)
-#this one not so much
+SET(CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_CROSSCOMPILE TRUE)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER /home/francois/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /home/francois/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER /home/francois/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /home/francois/x-tools/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
+set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH /home/francois/local/x-tools/arm-unknown-linux-gnueabi)
+SET(CMAKE_FIND_ROOT_PATH /home/francois/local/x-tools/arm-unknown-linux-gnueabihf)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-SET(CMAKE_CROSSCOMPILE YES)
+SET(THREADS_PTHREAD_ARG "0" CACHE STRING "Result from TRY_RUN" FORCE)
