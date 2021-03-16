7267d0194e17bf531974729e3c3696ae55c6ab98
francois <fitrec.jage@free.fr>
2021-03-16 23:00:56 +0100

    Config cross compile

--- builds/makepi/Toolchain-RaspberryPi.cmake
+++ builds/makepi/Toolchain-RaspberryPi.cmake
@@ -13,9 +13,11 @@ set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /home/rpi/rootfs)
-include_directories(${CMAKE_FIND_ROOT_PATH}/usr/local/include)
-link_directories(${CMAKE_FIND_ROOT_PATH}/usr/local/lib)
-link_directories(${CMAKE_FIND_ROOT_PATH}/lib/arm-linux-gnueabihf)
+SET(CMAKE_SYSROOT /home/rpi/rootfs)
+include_directories(${CMAKE_SYSROOT}/usr/local/include)
+link_directories(${CMAKE_SYSROOT}/usr/local/lib)
+
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} -Wl,-rpath-link=${CMAKE_SYSROOT}/lib" CACHE INTERNAL "" FORCE)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -23,6 +25,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 SET(THREADS_PTHREAD_ARG "0" CACHE STRING "Result from TRY_RUN" FORCE)
 
